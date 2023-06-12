525f5936fbeb3d961eebcc217ee361bbed034a36
kazvee <kayvee123@gmail.com>
2023-06-12 12:04:31 -0600

    Add server connection success confirmation message

--- play.js
+++ play.js
@@ -4,6 +4,9 @@ const connect = function() {
     host: 'localhost',
     port: 50541,
   });
+  conn.on('connect', () => {
+    console.log('Connected successfully! 😃');
+  });
   conn.setEncoding('utf8');
   return conn;
 };
